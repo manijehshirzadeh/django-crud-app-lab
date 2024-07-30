bd4177da9d5005bdc75e8727416560e86fffcc5c
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-07-30 21:50:43 +1000

    added urls for home and about. initialized the about view which extended a base template

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
+
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+def about(request):
+    # Send a simple HTML response
+    return render(request, 'about.html')
