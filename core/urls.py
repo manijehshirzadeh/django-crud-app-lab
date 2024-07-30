863192d87873f60874251fb2dd9bb42eb78c5914
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-07-30 22:49:01 +1000

    added home and books_index views. added styles.

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     path('', views.home, name='home'), # Routes will be added here
     path('about/', views.about, name='about'),
+    # route for books index
+    path('books/', views.book_index, name='book-index'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,14 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 
-# Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+class Book:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Book instances
+books = [
+    Book('Lolo', 'tabby', 'Kinda rude.', 3),
+    Book('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Book('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Book('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
 
 # Define the home view function
 def home(request):
     # Send a simple HTML response
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     # Send a simple HTML response
     return render(request, 'about.html')
+
+def book_index(request):
+    # Render the books/index.html template with the books data
+    return render(request, 'books/index.html', {'books': books})
