08aebf787191ae0fddf3af57abeda5c70932c246
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-07-31 17:36:32 +1000

    changed the model, and updated views accordingly

--- core/views.py
+++ core/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
+from .models import Book
 
-class Book:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Book instances
-books = [
-    Book('Lolo', 'tabby', 'Kinda rude.', 3),
-    Book('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Book('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Book('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
 
 # Define the home view function
 def home(request):
@@ -28,5 +13,6 @@ def about(request):
     return render(request, 'about.html')
 
 def book_index(request):
+    books = Book.objects.all()
     # Render the books/index.html template with the books data
     return render(request, 'books/index.html', {'books': books})
